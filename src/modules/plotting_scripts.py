1c0132e1e5b898dd30dd67a5febf12c4d982efab
NazaninShafiabadi <nazanin.shafiabadi97@gmail.com>
2024-07-31 21:03:49 +0200

    fitted regression line to MeanSurprisal in plots

--- src/modules/plotting_scripts.py
+++ src/modules/plotting_scripts.py
@@ -1,15 +1,35 @@
 """ Contains the functions needed for analyzing learning curves. """
 
 import math
-from typing import List
+from typing import List, Dict
 
-import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from scipy.stats import pearsonr
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 
 
-def plot_surprisals(words:List[str], surprisals_df, neg_samples=False, first_step=True):
+
+def fit_linear_model(X:np.ndarray, y:np.ndarray, first_step=True) -> Dict[str, float]:
+    if not first_step:
+        X, y = X[1:], y[1:]
+    model = LinearRegression().fit(X, y)
+    y_pred = model.predict(X)
+    alpha = model.coef_[0]  # slope
+    beta = model.intercept_
+    r2 = r2_score(y, y_pred)
+    mse = mean_squared_error(y, y_pred)
+    return X.flatten(), y_pred, {'alpha': alpha, 'beta': beta, 'R2': r2, 'MSE': mse}
+
+
+def plot_surprisals(words:List[str], surprisals_df, neg_samples=False, first_step=True, fit_line=False, return_outputs=False):
+    """ 
+    If first_step is set to False, neither the correlations nor the linear model will consider 
+    the first step, but the first step will still be shown on the plot.
+    """
     num_words = len(words)
     cols = min(num_words, 3)
     rows = math.ceil(num_words / cols)
@@ -19,6 +39,7 @@ def plot_surprisals(words:List[str], surprisals_df, neg_samples=False, first_ste
     axs = np.atleast_2d(axs)
 
     correlations = {}
+    all_metrics = {}
 
     for i, word in enumerate(words):   
         word_data = surprisals_df[surprisals_df['Token'] == word]
@@ -27,9 +48,27 @@ def plot_surprisals(words:List[str], surprisals_df, neg_samples=False, first_ste
             continue
 
         ax = axs[i//cols, i%cols]
+
+        metrics = {}
+
+        # Fit linear model for positive samples
+        X = word_data['Steps'].values.reshape(-1, 1)
+        y_pos = word_data['MeanSurprisal'].values
+        X_flat, y_pred_pos, metrics['positive'] = fit_linear_model(X, y_pos, first_step=first_step)
+        
+        # Plot positive surprisals & fitted line
+        ax.plot(word_data['Steps'], word_data['MeanSurprisal'], marker='o', color='darkseagreen', label='Positive Samples')
+        if fit_line:
+            ax.plot(X_flat, y_pred_pos, linestyle='--', color='#043927', label='Positive Fit')
+
         if neg_samples:
-            ax.plot(word_data['Steps'], word_data['MeanSurprisal'], marker='o', label='Positive Samples')
-            ax.plot(word_data['Steps'], word_data['MeanNegSurprisal'], marker='o', label='Negative Samples')
+            y_neg = word_data['MeanNegSurprisal'].values
+            X_flat, y_pred_neg, metrics['negative'] = fit_linear_model(X, y_neg, first_step=first_step)
+            
+            # Plot negative surprisals & fitted line
+            ax.plot(word_data['Steps'], word_data['MeanNegSurprisal'], marker='o', color='indianred', label='Negative Samples')
+            if fit_line:
+                ax.plot(X_flat, y_pred_neg, linestyle='--', color='#8D021F', label='Negative Fit')
 
             # Calculate correlation
             if not first_step:
@@ -41,13 +80,14 @@ def plot_surprisals(words:List[str], surprisals_df, neg_samples=False, first_ste
             ax.text(0.5, 1.02, f'Correlation: {corr:.2f}', fontsize=10, ha='center', transform=ax.transAxes)
 
         else:
-            ax.plot(word_data['Steps'], word_data['MeanSurprisal'], marker='o')
             ax.set_title(f'"{word}"')
         
         ax.set_xlabel('Steps')
         ax.set_ylabel('Mean surprisal')
         ax.invert_yaxis()
 
+        all_metrics[word] = metrics
+    
     # Remove empty subplots
     for j in range(i+1, rows*cols):
         fig.delaxes(axs.flatten()[j])
@@ -61,7 +101,10 @@ def plot_surprisals(words:List[str], surprisals_df, neg_samples=False, first_ste
     
     plt.show()
 
-    return correlations
+    if return_outputs:
+        return correlations, all_metrics
+    else: 
+        return
 
 
 def plot_all_in_one(words:List[str], surprisals_df):
@@ -138,3 +181,18 @@ def plot_avg_pos_neg(positives, negatives):
     plt.ylabel('Mean Surprisal')
     plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
     plt.show()
+
+
+def corr_plot(words:List[str], df, first_step=True) -> None:
+    """ The df needs to have individual surprisals """
+    for w in words:
+        w_data = df[df.Token == w].reset_index(drop=True)
+        w_count = w_data.groupby('Steps')['Token'].count().values[0]
+        X = w_data['Steps'].values.reshape(-1, 1)
+        y = w_data['Surprisal'].values
+        X_flat, y_pred, metrics = fit_linear_model(X, y, first_step=first_step)
+        g = sns.lineplot(data=w_data, x='Steps', y='Surprisal', errorbar=('ci', 100))
+        g.plot(X_flat, y_pred, linestyle='--', color='dimgray', label='Positive Fit')
+        g.figure.suptitle(f"{w} ({w_count} samples)")
+        g.text(0.5, 1.02, f'α: {metrics['alpha']:.2e}', fontsize=10, ha='center', transform=g.transAxes)
+        plt.show()
